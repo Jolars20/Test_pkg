3a5987d57c55cabfc2ff8e86068c73f19d16fc9c
J.B. Larsen <jolars20@student.aau.dk>
2020-10-20 10:28:42 +0200

    Updated README and test

--- src/Version_control_test.cpp
+++ src/Version_control_test.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <string.h>
 int main( int argc, char const *argv[])
 {
+    //make the code ask for a name and print "Understandable <name> have a nice day"
     std::cout << " This is a test of Git and Github for version control \n";
-
-    return 0;
+    std::cin << 
+    std::cout << "\n";
+return 0;
 }
